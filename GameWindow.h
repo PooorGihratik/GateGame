0399f2ce42a8f312051a25619ffb544fe8188f4c
Кир Кузнецов <kir.kuznetsov2013@gmail.com>
2021-01-19 20:46:28 +0300

    Added Main window file

---
+++ GameWindow.h
@@ -0,0 +1,31 @@
+//
+// Created by Кир on 17.01.2021.
+//
+
+#ifndef PROJECT_GAMEWINDOW_H
+#define PROJECT_GAMEWINDOW_H
+#include <list>
+#include <SFML/Graphics.hpp>
+#include "Abstractions/Abstractions.h"
+#include "Core/ComponentNetwork.h"
+
+using namespace std;
+using namespace sf;
+
+class GameWindow {
+private:
+    RenderWindow* window;
+    list<IController*> controllerList;
+    IController* focusedController = nullptr;
+    ComponentNetwork* network;
+    ICommand* command;
+public:
+    void init();
+    void updateLoop();
+    void render();
+    void checkWindowEvents(Event event);
+    void gotFocusEvent(IController* controller);
+    void lostFocusEvent();
+};
+
+#endif //PROJECT_GAMEWINDOW_H
